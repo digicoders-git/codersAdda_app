0cab2cc45710247e75531c67a14144dfd405c72f
Er.Mayank Pandey <mp04042007@gmail.com>
2026-08-02 09:40:24 +0530

    Fix DioException 401 on Cloudinary PDFs by replacing Dio with standard http client

--- lib/views/common/in_app_pdf_viewer_page.dart
+++ lib/views/common/in_app_pdf_viewer_page.dart
@@ -4,8 +4,7 @@ import 'package:coders_adda_app/utils/app_sizer/app_sizer.dart';
 import 'package:flutter/material.dart';
 import 'package:syncfusion_flutter_pdfviewer/pdfviewer.dart';
 import 'package:path_provider/path_provider.dart';
-import 'package:dio/dio.dart';
-import 'package:dio/io.dart';
+import 'package:http/http.dart' as http;
 
 class InAppPdfViewerPage extends StatefulWidget {
   final String pdfUrl;
@@ -23,7 +22,6 @@ class InAppPdfViewerPage extends StatefulWidget {
 
 class _InAppPdfViewerPageState extends State<InAppPdfViewerPage> {
   final GlobalKey<SfPdfViewerState> _pdfViewerKey = GlobalKey();
-  late Dio _dio;
   bool _isLoading = true;
   double _downloadProgress = 0;
   String? _localPath;
@@ -37,14 +35,7 @@ class _InAppPdfViewerPageState extends State<InAppPdfViewerPage> {
   }
 
   void _initDio() {
-    _dio = Dio();
-    _dio.httpClientAdapter = IOHttpClientAdapter(
-      createHttpClient: () {
-        final client = HttpClient();
-        client.badCertificateCallback = (X509Certificate cert, String host, int port) => true;
-        return client;
-      },
-    );
+    // Replaced Dio with http to fix 401 unauthorized Cloudinary issues
   }
 
   Future<void> _preparePdf() async {
@@ -80,17 +71,27 @@ class _InAppPdfViewerPageState extends State<InAppPdfViewerPage> {
         return;
       }
 
-      await _dio.download(
-        widget.pdfUrl,
-        _localPath,
-        onReceiveProgress: (count, total) {
+      final request = http.Request('GET', Uri.parse(widget.pdfUrl.trim()));
+      final response = await http.Client().send(request);
+
+      if (response.statusCode >= 200 && response.statusCode < 300) {
+        final total = response.contentLength ?? -1;
+        int count = 0;
+        final fileSink = file.openWrite();
+
+        await for (final chunk in response.stream) {
+          fileSink.add(chunk);
+          count += chunk.length;
           if (total != -1 && mounted) {
             setState(() {
               _downloadProgress = count / total;
             });
           }
-        },
-      );
+        }
+        await fileSink.close();
+      } else {
+        throw Exception('Server returned status code: ${response.statusCode}');
+      }
 
       if (mounted) {
         setState(() {
