6836a1f50191bb631c3234b644bd19230cc09b99
Er.Mayank Pandey <mp04042007@gmail.com>
2026-08-04 10:26:50 +0530

    feat: configure android build gradle with signing support and desugaring dependencies

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -49,7 +49,7 @@ android {
 
     buildTypes {
         release {
-            signingConfig = signingConfigs.getByName("release")
+            signingConfig = signingConfigs.getByName("debug")
         }
     }
 }
